1e255e090064352d373b03dca69c99a983f990f9
tingkc <master.chiew@gmail.com>
2020-05-16 21:08:49 +0800

    gl init boilerplate in place. Witness by changing the initial colors in gl_setup.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
+use web_sys::WebGlRenderingContext as GL;
+use web_sys::*;
+
+mod gl_setup;
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,19 +12,24 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub struct Client {}
+pub struct Client {
+    gl: WebGlRenderingContext,
+}
 
 #[wasm_bindgen]
 impl Client {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
         console_error_panic_hook::set_once();
-        Self {}
+        let gl = gl_setup::initialize_webgl_context().unwrap();
+        Self { gl: gl }
     }
 
     pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
         Ok(())
     }
 
-    pub fn render(&self) {}
+    pub fn render(&self) {
+        self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+    }
 }
